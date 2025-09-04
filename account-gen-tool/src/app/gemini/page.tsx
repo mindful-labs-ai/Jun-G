ee05c203f65c7accb09b22cf3d8865761d5e54da
KGyeong <rlarudals61@naver.com>
2025-09-04 22:45:59 +0900

    fix: 미사용 트리 쉐이킹

--- account-gen-tool/src/app/gemini/page.tsx
+++ account-gen-tool/src/app/gemini/page.tsx
@@ -4,7 +4,8 @@ import React from "react";
 const GeminiPage = () => {
   return (
     <>
-      <GeminiImageWithUpload />
+      <p>hello</p>
+      {/* <GeminiImageWithUpload /> */}
     </>
   );
 };
--- account-gen-tool/src/app/video/page.tsx
+++ account-gen-tool/src/app/video/page.tsx
@@ -4,7 +4,8 @@ import React from "react";
 const KlingPage = () => {
   return (
     <>
-      <GenerateVideoWithUpload />
+      <p>hello</p>
+      {/* <GenerateVideoWithUpload /> */}
     </>
   );
 };
